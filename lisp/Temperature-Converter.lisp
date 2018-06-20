c9358b983b0e6ae4283da48b9ddce30966359819
Marat Plotkin <mplot0139@gmail.com>
2018-06-19 21:38:03 -0400

    Wrote temp-in-range-p in lisp

--- lisp/Temperature-Converter.lisp
+++ lisp/Temperature-Converter.lisp
@@ -1,3 +1,23 @@
 ;;;; conversion between the values in Fahrenheit and Celsius
 ;;;; Lowest possible (valid) temperature is 459.67 F or 273.15
 ;;;; degrees Celsius - temperature of the absolute zero
+;;;; C = (F - 32) * 5/9
+;;;; F = C * 1.8 + 32
+
+;;; constants declaration
+(defconstant *abs-zero-fahr* 459.67)
+(defconstant *abs-zero-cels* 273.15)
+
+;;; checks if the temperature is above absolute zero
+;;; takes a temperature as a number and units as symbol
+;;; returns true or nil
+(define temp-in-range-p (temp units)
+	(if (and (symbolp units)
+		(numberp temp))
+	(cond ((eq units 'F)
+		(if (not (< temp *abs-zero-fahr*))
+			(* 5/9 (temp - 32))))
+	((eq units 'C)
+		(if (not (< temp *abs-zero-cels*))
+			(+ 32 (* 1.8 temp))))
+	(t nil))))
